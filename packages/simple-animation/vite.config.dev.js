2f1d2c3bc8297c7e28119fda19821ce22892df2f
Dean Vigoren <vigorator@gmail.com>
2023-09-05 14:34:19 -0600

    CI(simple-animation): A few changes for when running the dev server used to test things.

--- packages/simple-animation/vite.config.dev.js
+++ packages/simple-animation/vite.config.dev.js
@@ -1,44 +1,6 @@
-import { defineConfig } from "vite";
 import { resolve } from "path";
-import banner from "vite-plugin-banner";
 
-import { name, version, description, author } from "./package.json";
-
-export default defineConfig({
-    base: "./",
-    build: {
-        //outDir: "../dist",
-        emptyOutDir: true,
-        minify: true,
-        reportCompressedSize: true,
-        lib: {
-            entry: resolve(__dirname, "src/index.html"),
-            name: "SimpleAnimation",
-            fileName: "simple-animation"
-        },
-        rollupOptions: {
-            output: {
-                assetFileNames: `simple-animation.[ext]`
-            }
-        }
-    },
-    plugins: [
-        banner(`
-    ${name} v${version}: ${description}
-    Copyright (C) ${new Date().getFullYear()} ${author}
-                      
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-                       
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-                      
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see https://www.gnu.org/licenses/.
-`)
-    ]
-});
+import defaultConfig from "./vite.config.js";
+defaultConfig.build.lib.entry = resolve(__dirname, "src/index.html");
+defaultConfig.plugins = [];
+export default defaultConfig;
